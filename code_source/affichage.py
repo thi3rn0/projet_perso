c40e9f1b1f7723304c3441c8f8a1651ba23360f4
Thierno Galloro <thiernogal@gmail.com>
2022-12-22 11:32:08 +0100

    3e commit: ajout d'exception lors du choix des modules

--- code_source/affichage.py
+++ code_source/affichage.py
@@ -42,6 +42,7 @@ def afficher_liste_matieres_cp():
         affichage_liste_matieres[num_choix_matieres] = element[1]
         num_choix_matieres += 1
     # affichage permettant de choisir un numéro associé à une matière
+    print("Liste des matières: ")
     for matiere in affichage_liste_matieres.items():
         print('\t' + str(matiere[0]) + ") " + matiere[1])
 
@@ -59,6 +60,7 @@ def afficher_modules_maths_cp():
         affichage_modules_maths_cp[num_choix_modules_maths_cp] = element[1]
         num_choix_modules_maths_cp += 1
     # affichage permettant de choisir un numéro associé à une matière
+    print("Liste des modules: ")
     for module in affichage_modules_maths_cp.items():
         print('\t' + str(module[0]) + ") " + module[1])
 
--- code_source/main.py
+++ code_source/main.py
@@ -1,8 +1,9 @@
 from affichage import *
 
-nom_enfant = input("Entre ton nom: ")
+nom_enfant = str(input("Entre ton nom: "))
 afficher_accueil(nom_enfant)
-'\n'
+
+print("Liste des classes: ")
 liste_des_classes = afficher_liste_classes()
 choix_classe = int(input("choisis ta classe: "))
 
@@ -10,15 +11,18 @@ choix_classe = int(input("choisis ta classe: "))
 if choix_classe == 1:
     liste_des_matieres_cp = afficher_liste_matieres_cp()
     # choix de la matière
-    choix_matiere_cp = int(input("choisis ta matière: "))
+    try:
+        choix_matiere_cp = int(input("choisis ta matière: "))
 
-    if choix_matiere_cp == 1:
-        modules_maths_cp = afficher_modules_maths_cp()
-        choix_module_maths_cp = int(input("Choisis le module: "))
-        # affichage du conseil et de la ressource selon le module choisi
-        affichage_conseil_maths_cp(choix_module_maths_cp)
+        if choix_matiere_cp == 1:
+            modules_maths_cp = afficher_modules_maths_cp()
+            choix_module_maths_cp = int(input("Choisis le module: "))
+            # affichage du conseil et de la ressource selon le module choisi
+            affichage_conseil_maths_cp(choix_module_maths_cp)
 
-    else:
-        pass
+        else:
+            pass
+    except ValueError:
+        print("Merci de choisir un nombre parmi la liste.")
 else:
     pass
